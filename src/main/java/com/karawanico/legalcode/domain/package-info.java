fe7aa6c21d113ef03f001509c0c93d59e81386e1
Joyal William <williamjefferson225@gmail.com>
2020-06-30 12:23:07 +0400

    Generated with JHipster 6.7.1

---
+++ src/main/java/com/karawanico/legalcode/domain/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * JPA domain objects.
+ */
+package com.karawanico.legalcode.domain;
---
+++ src/main/java/com/karawanico/legalcode/repository/search/UserSearchRepository.java
@@ -0,0 +1,10 @@
+package com.karawanico.legalcode.repository.search;
+
+import com.karawanico.legalcode.domain.User;
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+
+/**
+ * Spring Data Elasticsearch repository for the User entity.
+ */
+public interface UserSearchRepository extends ElasticsearchRepository<User, Long> {
+}
